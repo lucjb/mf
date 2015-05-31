6245ff441a6920f2e365cf580ec9049e1ccc2343
Lucas <lucejb@gmail.com>
2015-05-31 22:05:35 +0200

    added model persistence

--- online-mf.py
+++ online-mf.py
@@ -1,41 +1,49 @@
 import csv
 import numpy
 import math
+import random
 
-D = 2 ** 18
-
-def hash_trick(x):
-	return abs(hash(x)) % D
+def hash_trick(x, D):
+	return abs(hash(x)) % D 
 
 def mean_rate(file_name):
+	users = set()	
+	items = set()
 	ex_count = 0
 	f = open(file_name)
 	reader = csv.reader(f,  delimiter='\t')
 	r_sum = 0
-	for i, row in enumerate(reader):
-		u, i, r = hash_trick(int(row[0])), hash_trick(int(row[1])), int(row[2])
+	for row in reader:
+		user = row[0]
+		item = row[1]
+		users.add(user)
+		items.add(item)
+		r = int(row[2])
 		r_sum += r
 		ex_count += 1 	
 	mu = r_sum/float(ex_count)
 	f.close()
 	
-	return mu
+	return mu, users,items
 
-def matrix_factorization(file_name, K = 10, passes=20, gamma=0.015, lambda0=0.1):
-
+def matrix_factorization(file_name, K = 10, passes=1, gamma=0.015, lambda0=0.1, B=18):
+	D = 2 ** B
 	P = numpy.float64(numpy.random.rand(D, K))
 	Q = numpy.float64(numpy.random.rand(K, D))
-	BU = [0]*D
-	BI = [0]*D
+	BU = [0.]*D
+	BI = [0.]*D
+	for i in range(0, D):
+		BU[i] = random.random()
+		BI[i] = random.random()
+	mu, users, items = mean_rate(file_name)
 
-	mu = mean_rate(file_name)
 	loss = 0
 	ex_count = 0
 	for pass_n in range(0, passes):
 		f = open(file_name)
 		reader = csv.reader(f,  delimiter='\t')
-		for i, row in enumerate(reader):
-			u, i, r = hash_trick(int(row[0])), hash_trick(int(row[1])), int(row[2])
+		for row in reader:
+			u, i, r = hash_trick(int(row[0]), D), hash_trick(int(row[1]), D), int(row[2])
 
 			eui = r - predict(mu, BU[u], BI[i], P[u,:], Q[:,i])
 
@@ -44,17 +52,16 @@ def matrix_factorization(file_name, K = 10, passes=20, gamma=0.015, lambda0=0.1)
 
 			BU[u]+=gamma*(eui-lambda0*BU[u])
 			BI[i]+=gamma*(eui-lambda0*BI[i])	
-
 			puo = P[u,:]
 			qio = Q[:,i]
 
 			P[u,:]+=gamma*(eui*qio - lambda0*puo)
 			Q[:,i]+=gamma*(eui*puo - lambda0*qio)
 
-			if ex_count % 10000==0:
+			if ex_count % 10000==0:	
 				print loss/float(ex_count)
 		f.close()
-	return mu, BU, BI, P, Q
+	return mu, BU, BI, P, Q, users, items
 
 def predict(mu, BUi, BIj, Pi, Qj):
 	pred = 0
@@ -63,14 +70,15 @@ def predict(mu, BUi, BIj, Pi, Qj):
 	pred += numpy.dot(Pi, Qj)
 	return pred
 
-def validate(file_name, mu, BU, BI, P, Q):
+def validate(file_name, mu, BU, BI, P, Q, B):
+	D = 2**B
 	loss = 0.
 	ex_count = 0.
 	f = open(file_name)
 	reader = csv.reader(f,  delimiter='\t')
 	r_sum = 0
-	for i, row in enumerate(reader):
-		u, i, r = hash_trick(int(row[0])), hash_trick(int(row[1])), int(row[2])
+	for row in reader:
+		u, i, r = hash_trick(int(row[0]), D), hash_trick(int(row[1]), D), int(row[2])
 		eui = r - predict(mu, BU[u], BI[i], P[u,:], Q[:,i])
 		loss += eui**2
 		ex_count += 1		
@@ -82,11 +90,56 @@ def validate(file_name, mu, BU, BI, P, Q):
 
 if __name__ == "__main__":
 	numpy.seterr(all='raise')
-	mu, BU, BI, P, Q = matrix_factorization('ua.base')
-	validate('ua.test', mu, BU, BI, P, Q)	
-	validate('ub.test', mu, BU, BI, P, Q)	
+	K = 10
+	passes=20
+	gamma=0.015
+	lambda0=0.1
+	B=18
+	
+	mu, BU, BI, P, Q, users, items = matrix_factorization('ua.base', K, passes, gamma, lambda0, B)
+	
 
-# K = 10, passes=20, gamma=0.015, lambda0=0.1, ua.base, ua.test
-#Total Loss (SE): 8497.244551, mean loss (MSE): 0.901086, RMSE: 0.949256
+	D = 2**B
+	f = open('mf.model', 'w')
+	f.write("B, %d\n" % B)
+	f.write("K, %d\n" % K)
+	f.write("mu, %f\n" % mu)
 
+	for user in users:
+		u = hash_trick(user, D)
+		line_u = "%s, %f, " % (user, BU[u])
+		for w in P[u]:
+			line_u += "%f, " % w			
+		line_u = line_u[:-2]+"\n"
+		f.write(line_u)
+
+	for item in items:
+		i = hash_trick(item, D)
+		line_i = "%s, %f, " % (item, BI[i])
+		for w in Q[:,i]:
+			line_i += "%f, " % w			
+		line_i = line_i[:-2]+"\n"
+		f.write(line_i)
+	
+	f.close()
+		
+	print 'Training error:'
+	validate('ua.base', mu, BU, BI, P, Q, B)
+	print 'Held out set error:'	
+	validate('ua.test', mu, BU, BI, P, Q, B)	
+	validate('ub.test', mu, BU, BI, P, Q, B)	
+
+'''
+K = 10
+passes=20
+gamma=0.015
+lambda0=0.1
+B=18
+
+ua.base Total Loss (SE): 60711.228088, mean loss (MSE): 0.670324, RMSE: 0.818733
+Held out set error:
+ua.test Total Loss (SE): 8486.100675, mean loss (MSE): 0.899905, RMSE: 0.948633
+ub.test Total Loss (SE): 6243.402562, mean loss (MSE): 0.662079, RMSE: 0.813682
+
+''' 
 
